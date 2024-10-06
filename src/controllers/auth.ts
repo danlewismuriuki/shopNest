802da25a6054a4b37b0c63f78f05ca70dff54986
lsd <danlewismuriuki2@gmail.com>
2024-10-06 23:22:47 +0300

    feat: implement signUp function for user registration
    - Added the signUp function to handle user registration.
    - Checks if the user already exists based on the email.
    - If not, creates a new user with hashed password and returns user data.

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,7 +1,21 @@
 import { Request, Response } from 'express'
+import { PrismaClient } from '..';
+import { hashSync } from 'bcrypt';
 
-export const signUp = (req: Request, rest: Response) => {
+export const signUp = async (req: Request, res: Response) => {
     const { email, password, name } = req.body;
+    let user = await PrismaClient.user.findFirst({ where: { email } })
+    if (user) {
+        throw Error('User already exists!')
+    }
+    user = await PrismaClient.user.create({
+        data: {
+            name,
+            email,
+            password: hashSync(password, 10)
+        }
+    })
+    res.json(user)
 }
 
 export default signUp;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { login } from '../controllers/auth'
+import { signUp } from '../controllers/auth'
 import authRoutes from './auth';
 
 const rootRouter: Router = Router()
