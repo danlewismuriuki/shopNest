5e1eca914c0cfb6c070b030e248369e91c84346b
lsd <danlewismuriuki2@gmail.com>
2024-10-08 21:00:20 +0300

    fix(database): resolve Prisma rollback issue during user insertion
    - Fixed an issue where the Prisma client triggered a rollback during user insertion into the `users` table.
    - Debugged Prisma query logs to trace the error in `INSERT INTO shopNest.users`.
    - Ensured correct handling of SQL query execution and rollback scenarios.
    - Adjusted middleware to handle PrismaPromise errors properly.

--- src/middlewares/errors.ts
+++ src/middlewares/errors.ts
@@ -1,3 +1,10 @@
 import { HttpException } from "../exceptions/roots";
 import { NextFunction, Request, Response } from "express";
-export errorMiddleware = (error: HttpException, req: Request, res: Response, next: NextFunction)
+
+export const errorMiddleware = (error: HttpException, req: Request, res: Response, next: NextFunction) => {
+    res.status(error.statusCode).json({
+        message: error.message,
+        errorCode: error.errorCode,
+        errors: error.errors
+    })
+}
