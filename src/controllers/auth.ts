12296edea264ec05ec4095462ee5317c2993747a
lsd <danlewismuriuki2@gmail.com>
2024-10-09 00:17:55 +0300

    feat(auth): add user retrieval by email with Prisma
    - Implemented user query to fetch user details by email using Prisma ORM.
    - Updated TypeScript code to handle the query result.
    - Verified correct response from the database and server is running on port 3000.

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -9,7 +9,8 @@ export const signUp = async (req: Request, res: Response, next: NextFunction) =>
     const { email, password, name } = req.body;
     let user = await PrismaClient.user.findFirst({ where: { email } })
     if (user) {
-        next(new BadRequestsException('User already Exist!', ErrorCode.USER_ALREADY_EXISTS))
+        next(new BadRequestsException('User already Exist!', ErrorCode.USER_ALREADY_EXISTS));
+        return;
     }
     user = await PrismaClient.user.create({
         data: {
