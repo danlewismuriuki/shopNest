5cf9e1b920690650c586d2657054818afc469fdc
lsd <danlewismuriuki2@gmail.com>
2024-10-12 02:32:42 +0300

    feat: add centralized error handling middleware
    - Implement errorMiddleware to handle and format all exceptions thrown in the application
    - Ensure consistent error response structure with message, errorCode, and errors
    - Set appropriate HTTP status codes in the response based on error type (e.g., 404, 500)
    - Integrate with custom HttpException class for better error management

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -7,28 +7,24 @@ import { ErrorCode } from '../exceptions/roots';
 import { NotFoundException } from '../exceptions/not-found';
 import { IncorrectPassException } from '../exceptions/incorrect-pass';
 import { SignUpSchema } from '../schema/users';
-import { UprocessableEntity } from '../exceptions/validation';
 
 export const signUp = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        SignUpSchema.parse(req.body)
-        const { email, password, name } = req.body;
-        let user = await PrismaClient.user.findFirst({ where: { email } })
-        if (user) {
-            next(new BadRequestsException('User already Exist!', ErrorCode.USER_ALREADY_EXISTS));
-            return;
-        }
-        user = await PrismaClient.user.create({
-            data: {
-                name,
-                email,
-                password: hashSync(password, 10)
-            }
-        })
-        res.json(user)
-    } catch (err: any) {
-        next(new UprocessableEntity(err?.issues, 'Unprocessed entity', ErrorCode.UNPROCESSABLE_ENTITY))
+    SignUpSchema.parse(req.body)
+    const { email, password, name } = req.body;
+
+    let user = await PrismaClient.user.findFirst({ where: { email } })
+    if (user) {
+        next(new BadRequestsException('User already Exist!', ErrorCode.USER_ALREADY_EXISTS));
+        return;
     }
+    user = await PrismaClient.user.create({
+        data: {
+            name,
+            email,
+            password: hashSync(password, 10)
+        }
+    })
+    res.json(user)
 }
 
 export const login = async (req: Request, res: Response, next: NextFunction) => {
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express'
 import { signUp, login } from '../controllers/auth'
+import { errorHandler } from '../error-handler'
 
 const authRoutes: Router = Router()
 
-authRoutes.post('/signUp', signUp)
-authRoutes.post('/login', login)
+authRoutes.post('/signUp', errorHandler(signUp))
+authRoutes.post('/login', errorHandler(login))
 
 export default authRoutes
