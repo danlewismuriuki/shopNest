c19db000be76a5928fdeeb443490ae4355be84e9
lsd <danlewismuriuki2@gmail.com>
2024-10-16 21:25:43 +0300

    feat: Implement authentication middleware to verify JWT tokens
    - Added middleware to extract JWT from request headers and verify it against the JWT_SECRET.
    - Implemented error handling for missing tokens and undefined secrets.
    - Retrieved user information from the database using the user ID from the token payload.
    - Attached user object to the request for downstream access.
    - Ensured unauthorized access is handled gracefully with appropriate exceptions.

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-import { UnauthorzedException } from "../exceptions/unauthorized";
+import { UnauthorizedException } from "../exceptions/unauthorized";
 import { ErrorCode } from "../exceptions/roots";
 import * as jwt from 'jsonwebtoken';
 import { JWT_SECRET } from "../secrets";
@@ -11,20 +11,24 @@ const authMiddleware = async(req: Request, res:Response, next:NextFunction) => {
 const token = req.headers.authorization
 // if toen not present throw an error of Unauthorized
 if(!token) {
-    next(new UnauthorzedException('Unauthorized', ErrorCode.UNAUTHORIZED))
+    next(new UnauthorizedException('Unauthorized', ErrorCode.UNAUTHORIZED));
+    return;
+}
+if (!JWT_SECRET) {
+    return next(new UnauthorizedException('JWT secret is not defined', ErrorCode.UNAUTHORIZED));
 }
 // if token is present, verify that token and extract the payload
 
 try {
-    const payload: { userId:number} = jwt.verify(token, JWT_SECRET)
+    const payload  = jwt.verify(token, JWT_SECRET) as any
     const user = await PrismaClient.user.findFirst({where: {id: payload.userId}})
     if (!user) {
-        next(new UnauthorzedException('Unauthorized', ErrorCode.UNAUTHORIZED))
+        next(new UnauthorizedException('Unauthorized', ErrorCode.UNAUTHORIZED))
     }
-
-
+    req.user = user
+    next()
 } catch(error) {
-    next(new UnauthorzedException('Unauthorized', ErrorCode.UNAUTHORIZED))
+    next(new UnauthorizedException('Unauthorized', ErrorCode.UNAUTHORIZED))
 }
 
 }
