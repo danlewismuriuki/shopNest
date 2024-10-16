5a1cc81d246ee97a3f715bcb54459f126948d10a
lsd <danlewismuriuki2@gmail.com>
2024-10-16 18:38:39 +0300

    feat(auth): Attach user object to request after successful login
    - Implemented middleware to verify JWT and retrieve user data from the database.
    - Populated req.user with user details (id, name, email, createdAt, updatedAt) while excluding sensitive information like password.
    - Improved request handling to provide structured user information throughout the request lifecycle.

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -3,10 +3,29 @@ import { UnauthorzedException } from "../exceptions/unauthorized";
 import { ErrorCode } from "../exceptions/roots";
 import * as jwt from 'jsonwebtoken';
 import { JWT_SECRET } from "../secrets";
+import { Prisma } from "@prisma/client";
+import { PrismaClient } from "..";
 
 const authMiddleware = async(req: Request, res:Response, next:NextFunction) => {
 //1. extract the token from header
+const token = req.headers.authorization
+// if toen not present throw an error of Unauthorized
+if(!token) {
+    next(new UnauthorzedException('Unauthorized', ErrorCode.UNAUTHORIZED))
+}
+// if token is present, verify that token and extract the payload
 
+try {
+    const payload: { userId:number} = jwt.verify(token, JWT_SECRET)
+    const user = await PrismaClient.user.findFirst({where: {id: payload.userId}})
+    if (!user) {
+        next(new UnauthorzedException('Unauthorized', ErrorCode.UNAUTHORIZED))
+    }
+
+
+} catch(error) {
+    next(new UnauthorzedException('Unauthorized', ErrorCode.UNAUTHORIZED))
 }
 
+}
 export default authMiddleware;
