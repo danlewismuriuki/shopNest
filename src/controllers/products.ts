1cdd0766eb097b3acc920e0d7390014ec5fe96b6
lsd <danlewismuriuki2@gmail.com>
2024-10-19 04:21:12 +0300

    Refactor updateProduct controller to handle tag conversion and update product by ID. Added proper error handling for missing products.

--- src/controllers/products.ts
+++ src/controllers/products.ts
@@ -1,12 +1,14 @@
    import { Request, Response } from "express";
    import { PrismaClient } from "..";
+import { NotFoundException } from "../exceptions/not-found";
+import { ErrorCode } from "../exceptions/roots";
    
 
    export const createProduct = async(req:Request, res:Response) => {
     
     //create a validator for the request
 
-    
+
     console.log("Request body:", req.body);
     const product = await PrismaClient.product.create({
         data: {
@@ -15,4 +17,34 @@
         }
     })
     res.json(product)
+   }
+
+   export const updateProduct = async (req: Request, res: Response) => {
+    try {
+        const product = req.body;
+        if (product.tags) {
+            product.tags = product.tags.join(',')
+        }
+        const updateProduct = await PrismaClient.product.update({
+            where : {
+                id : +req.params.id
+            },
+            data: product
+        })
+        res.json(updateProduct)
+    } catch(err) {
+        throw new NotFoundException('Product not found.', ErrorCode.PRODUCT_NOT_FOUND)
+    }
+   }
+
+   export const deleteProduct = async (req: Request, res: Response) => {
+
+   }
+
+   export const listProducts = async (req: Request, res: Response) => {
+
+   }
+
+   export const getProductById = async (req: Request, res: Response) => {
+
    }
