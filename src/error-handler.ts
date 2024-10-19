a194ef7eec4a682bcbb1eb400352093b6b844a82
lsd <danlewismuriuki2@gmail.com>
2024-10-19 22:25:27 +0300

    fix: pass Zod validation errors correctly to BadRequestsException
    - Ensure ZodError details are properly passed as 'errors' in BadRequestsException.
    - Modify error handling to pass error.errors for better clarity in responses.
    - Fixed issue where "errors" field was returning null instead of detailed validation messages.

--- src/error-handler.ts
+++ src/error-handler.ts
@@ -1,6 +1,8 @@
 import { Request, Response, NextFunction } from "express";
 import { HttpException, ErrorCode } from "./exceptions/roots";
 import { InternalException } from "./exceptions/internal-exception";
+import { ZodError } from "zod";
+import { BadRequestsException } from "./exceptions/bad-request";
 
 export const errorHandler = (method: Function) => {
     return async (req: Request, res: Response, next: NextFunction) => {
@@ -10,8 +12,12 @@ export const errorHandler = (method: Function) => {
             let exception: HttpException;
             if (error instanceof HttpException) {
                 exception = error;
-            } else {
-                exception = new InternalException('Something went Wrong!', error, ErrorCode.INTERNAL_EXCEPTION)
+            }  else {
+                if ( error instanceof ZodError) {
+                    exception = new BadRequestsException('Unprocessable entity', ErrorCode.UNPROCESSABLE_ENTITY, error)
+                } else {
+                    exception = new InternalException('Something went Wrong!', error, ErrorCode.INTERNAL_EXCEPTION)
+                }
             }
             next(exception)
         }
