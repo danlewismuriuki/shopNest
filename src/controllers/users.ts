7bc2e92b30a2d80850ded0ff121f73d0c9ddbd25
lsd <danlewismuriuki2@gmail.com>
2024-10-20 17:36:54 +0300

    feat: implement address deletion functionality
    - Added deleteAddress controller to handle DELETE requests.
    - Extracted the address ID from the URL parameters.
    - Implemented logic to delete the address using PrismaClient.
    - Return success response if deletion is successful.
    - Throw NotFoundException if the address is not found.

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -19,7 +19,16 @@ export const addAddress  =async(req: Request, res:Response) => {
 }
 
 export const deleteAddress = async(req: Request, res:Response) => {
-    
+    try {
+        await PrismaClient.address.delete({
+            where : {
+                id: +req.params.id
+            }
+        })
+        res.json({success: true})
+    } catch (err) {
+        throw new NotFoundException('Address not found', ErrorCode.ADRESS_NOT_FOUND)
+    }
 }
 
 export const listAddress = async(req: Request, res:Response) => {
--- src/exceptions/roots.ts
+++ src/exceptions/roots.ts
@@ -18,6 +18,7 @@ export class HttpException extends Error {
 export enum ErrorCode {
     USER_NOT_FOUND = 1001,
     USER_ALREADY_EXISTS = 1002,
+    ADRESS_NOT_FOUND = 1004,
     INCORRECT_PASSWORD = 1003,
     UNPROCESSABLE_ENTITY = 2001,
     INTERNAL_EXCEPTION = 3005,
