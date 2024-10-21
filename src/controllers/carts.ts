9da16151e1bad6766aafd8c680b3b4186d2f9857
lsd <danlewismuriuki2@gmail.com>
2024-10-21 18:10:16 +0300

    feat(cart): implement addItemToCart functionality
    - Validates incoming request data for adding items to the cart
    - Checks for the existence of the specified product
    - Creates a new cart item for the authenticated user
    - Returns the newly created cart item in the response

--- src/controllers/carts.ts
+++ src/controllers/carts.ts
@@ -1,6 +1,30 @@
 import { Request, Response } from "express";
+import { CreateCartSchema } from "../schema/cart";
+import { Product } from "@prisma/client";
+import { PrismaClient } from "..";
+import { NotFoundException } from "../exceptions/not-found";
+import { ErrorCode } from "../exceptions/roots";
 
 export const addItemToCart = async (req: Request, res: Response) => {
+    const validatedData = CreateCartSchema.parse(req.body)
+    let product: Product;
+    try {
+        product = await PrismaClient.product.findFirstOrThrow({
+            where: {
+                id : validatedData.productId
+            }
+        })
+    } catch(err) {
+        throw new NotFoundException('Product not found!', ErrorCode.PRODUCT_NOT_FOUND)
+    }
+    const cart = await PrismaClient.cartItem.create({
+        data: {
+            userId: req.user.id,
+            productId: product.id,
+            quantity: validatedData.quantity
+        }
+    })
+    res.json(cart)
 }
 
 export const deleteItemFromCart = async (req: Request, res: Response) => {
