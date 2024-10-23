a45ee4a6b486bcc4086eeece1d4bfaa132664ff0
lsd <danlewismuriuki2@gmail.com>
2024-10-23 18:06:16 +0300

    Added documentation to the product routes

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -5,7 +5,7 @@ import authMiddleware from '../middlewares/auth';
 import adminMiddleware from '../middlewares/admin';
 
 const productsRoutes:Router = Router()
-
+// Product routes 
 productsRoutes.post('/',[authMiddleware, adminMiddleware], errorHandler(createProduct))
 productsRoutes.put('/:id',[authMiddleware, adminMiddleware], errorHandler(updateProduct))
 productsRoutes.delete('/:id',[authMiddleware, adminMiddleware], errorHandler(deleteProduct))
